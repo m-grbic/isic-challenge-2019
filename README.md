1ca6d6877a9b8201fd7403c135ba6bd5e684ecb3
Mihailo Grbic <mihailogrbic98@gmail.com>
2024-06-01 14:23:00 +0200

    feat: visualization of learning curves, augmentation transforms, README update

--- README.md
+++ README.md
@@ -8,6 +8,14 @@ Library `timm` is used for ViT and ConvNeXt models, and the `efficientnet_pytorc
 pip install efficientnet_pytorch timm
 ```
 
+## Number of parameters
+
+
+| use_metadata\model | ConvNext | ViT | EfficientNet | ResNet50 | ResNet34 | 
+|---|---|---|---|---|---|
+| false | 87.57M | 85.8M | 4.02M | 23.52M | 21.29M
+| true  | 87.64M | 85.86M | 4.1M | 23.65M | 21.33M
+
 
 ## Inference speed benchmark
 
@@ -17,3 +25,12 @@ Benchmark was done on NVIDIA GeForce RTX 4070 Super with batch size 128 and 10 w
 |---|---|---|---|---|---|
 | false | 403 | 456 | 632 | 640 | 893
 | true  | 394 | 468 | 678 | 622 | 879
+
+
+
+## Convergence time
+
+
+| model | ConvNext | ViT | EfficientNet | ResNet50 | ResNet34 | 
+|---|---|---|---|---|---|
+| time | 1h 50min | 1h 6min | 33min | 37min | 21min
--- src/utilities/transforms.py
+++ src/utilities/transforms.py
@@ -2,13 +2,27 @@ from torchvision import transforms
 
 
 def get_train_transforms():
-    # Data preprocessing and augmentation
     return transforms.Compose([
-        transforms.Resize((256, 256)),
-        transforms.RandomResizedCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize with ImageNet stats
+    transforms.RandomRotation(30),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomVerticalFlip(),
+    transforms.RandomResizedCrop(size=224, scale=(0.8, 1.0)),
+    transforms.RandomAffine(degrees=0, translate=(0.1, 0.1), scale=(0.9, 1.1), shear=10),
+    transforms.ColorJitter(brightness=0.2, contrast=0.2),  # Brightness and contrast adjustment
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
+
+
+# def get_train_transforms():
+#     # Data preprocessing and augmentation
+#     return transforms.Compose([
+#         transforms.Resize((256, 256)),
+#         transforms.RandomResizedCrop(224),
+#         transforms.ToTensor(),
+#         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize with ImageNet stats
+#     ])
+
 
 def get_test_transforms():
     # Data preprocessing and augmentation
